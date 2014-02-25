722db7988c225889371fcb850fd28dded45e513f
Robin Miller & Ben Birdsall <student@epicodus.com>
2014-02-24 17:18:45 -0800

    more flailing that does not overcome event listener limitations

--- js/scripts.js
+++ js/scripts.js
@@ -15,12 +15,12 @@ $(document).ready(function() {
     newContact.firstName = inputtedFirstName;
     newContact.lastName = inputtedLastName;
     newContact.address = inputtedAddress;
-
     
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
     $(".contact").last().click(function() {
       $("#contact-display").show();
+      $("#update-contact").hide();
 
       $("#update-contact h2").text(newContact.fullName());
       $("#contact-display h2").text(newContact.fullName());
@@ -28,34 +28,33 @@ $(document).ready(function() {
       $("#existing-last-name").text(newContact.lastName);
       $("#existing-address").text(newContact.address);
 
-      this.reset();
+      $("button#update-button").click(function() {
+        $("#contact-display").toggle();
+        $("#update-contact").toggle();
+
+        $("input#edit-first-name").val(newContact.firstName);
+        $("input#edit-last-name").val(newContact.lastName);
+        $("input#edit-address").val(newContact.address);
+
+        $("form#edit-contact").submit(function(event) {
+          event.preventDefault();
+
+          newContact.firstName = $("input#edit-first-name").val();
+          newContact.lastName = $("input#edit-last-name").val();
+          newContact.address = $("input#edit-address").val();
+          $("#existing-first-name").text(newContact.firstName);
+          $("#existing-last-name").text(newContact.lastName);
+          $("#existing-address").text(newContact.address);      
+
+          $("#contact-display").toggle();
+          $("#update-contact").toggle();
+
+          this.reset();
+        });
+      });
+
+      
     });
-
-    $("button#update-button").click(function() {
-      $("#contact-display").toggle();
-      $("#update-contact").toggle();
-
-      $("input#edit-first-name").val(newContact.firstName);
-      $("input#edit-last-name").val(newContact.lastName);
-      $("input#edit-address").val(newContact.address);
-    });
-
-    $("form#edit-contact").submit(function(event) {
-      event.preventDefault();
-
-      newContact.firstName = $("input#edit-first-name").val();
-      newContact.lastName = $("input#edit-last-name").val();
-      newContact.address = $("input#edit-address").val();
-      $("#existing-first-name").text(newContact.firstName);
-      $("#existing-last-name").text(newContact.lastName);
-      $("#existing-address").text(newContact.address);      
-
-      $("#contact-display").toggle();
-      $("#update-contact").toggle();
-
-      this.reset();
-    });
-
-  });
+  });   
 });
   
